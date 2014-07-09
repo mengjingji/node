293c754c2097871986922dbe47f3d5d4e4239a7a
mengjingji <mengjingji@126.com>
2014-07-09 15:26:25 +0800

    node io study

---
+++ study/io/parser.js
@@ -0,0 +1,57 @@
+// Parser constructor.  
+var Parser = function() {  
+  
+};  
+  
+// Parses the specified text.  
+Parser.prototype.parse1 = function(text) {
+  
+  var results = {};  
+  
+  // Break up the file into lines.  
+  var lines = text.split('\n');  
+  
+  lines.forEach(function(line) {  
+    var parts = line.split(' ');  
+    var letter = parts[1];  
+    var count = parseInt(parts[2]);  
+  
+    if(!results[letter]) {  
+      results[letter] = 0;  
+    }  
+  
+    results[letter] += parseInt(count);  
+  });  
+  
+  return results;  
+};  
+
+var P = function() {  
+  
+};  
+P.prototype.parse=function(text) {
+  
+  var results = {m:"mjj"};  
+  
+  // Break up the file into lines.  
+  var lines = text.split('\n');  
+  
+  lines.forEach(function(line) {  
+    var parts = line.split(' ');  
+    var letter = parts[1];  
+    var count = parseInt(parts[2]);  
+  
+    if(!results[letter]) {  
+      results[letter] = 0;  
+    }  
+  
+    results[letter] += parseInt(count);  
+  });  
+  
+  return results;  
+};    
+// Export the Parser constructor from this module.  
+
+
+module.exports = Parser;
+module.exports = P;
